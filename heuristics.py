6852f8e23b5034eb0f971b7e93a3a03dbf0e66d7
Karol Kita <kkita970@gmail.com>
2025-03-25 21:34:10 +0100

    implementation best fit algorithm

--- heuristics.py
+++ heuristics.py
@@ -27,3 +27,24 @@ def first_fit(weight, c):
             bin_rem[res] = c - weight[i]
             res = res + 1
     return res
+
+
+def best_fit(weight, c):
+    n = len(weight)
+    res = 0
+    bin_rem = [0] * n
+
+    for i in range(n):
+        min = c + 1
+        bi = 0
+        for j in range(res):
+            if (bin_rem[j] >= weight[i] and bin_rem[j] - weight[i] < min):
+                bi = j
+                min = bin_rem[j] - weight[i]
+
+        if (min == c + 1):
+            bin_rem[res] = c - weight[i]
+            res += 1
+        else:
+            bin_rem[bi] -= weight[i]
+    return res
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from heuristics import nextfit, first_fit
+from heuristics import nextfit, first_fit, best_fit
 
 weight = [2, 5, 4, 7, 1, 3, 8]
 c = 10
 
 print(f'Number of bins required in Next Fit : {nextfit(weight, c)}')
 print(f'Number of bins required in First Fit : {first_fit(weight, c)}')
+print(f'Number of bins required in Best Fit : {best_fit(weight, c)}')
