e314bf5515c5c7a44df6b7ba8166c970fd9ef022
Karina <karina.krotkiewicz@gmail.com>
2025-03-26 18:40:20 +0100

    Add methods testing

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from heuristics import nextfit, first_fit, best_fit
+import time
 
-weight = [2, 5, 4, 7, 1, 3, 8]
-c = 10
 
-print(f'Number of bins required in Next Fit : {nextfit(weight, c)}')
-print(f'Number of bins required in First Fit : {first_fit(weight, c)}')
-print(f'Number of bins required in Best Fit : {best_fit(weight, c)}')
+
+weight1 = [4, 8, 1, 4, 2, 1, 10, 3, 5, 6, 2, 7, 5, 4, 3, 6, 4, 2, 6, 7, 2]
+c1 = 10
+
+datasets = {
+    'Dataset_1': (weight1, c1)
+}
+methods = {
+    'Next_fit': nextfit,
+    'First_fit': first_fit,
+    'Best_fit': best_fit
+}
+
+
+
+
+for dataset_name, dataset in datasets.items():
+    weights = dataset[0]
+    bin_with_capacity = dataset[1]
+    for method_name, method in methods.items():
+        start_time = time.perf_counter()
+        method_output = method(weights, bin_with_capacity)
+        # elapsed time ms
+        elapsed_time = (time.perf_counter() - start_time) * 1000
+        output = f"{dataset_name};{method_name};{method_output};{elapsed_time}\n"
+        with open('output.csv', 'a') as file:
+            file.write(output)
+        print(output)
+
